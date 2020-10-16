2dcfeb90df8e2319d8292a0bc3047eb4267002a7
Kaye Engbino <engbinop@gmail.com>
2020-10-16 13:26:13 +0800

    Design Home Page and Manage Lists Page

---
+++ PHP_Deliverable/index.php
@@ -0,0 +1,48 @@
+<html>
+	<head>
+		<title>No Shilly-Shally</title>
+	</head>
+	
+	<body>
+		<h1>NO SHILLY-SHALLY</h1>
+	
+		<!-- Menu Starts Here-->
+		<div class="menu">
+			<a href="index.php">HOME</a>	<!-- Static Link -->
+		
+			<!-- Dynamic Links -->
+			<a href="#">To Do</a>
+			<a href="#">In Progress</a>
+			<a href="#">Done</a>
+
+			<a href="manage_list.php">Manage Lists</a>	<!-- Static Link -->
+		</div>
+		<!-- Menu Ends Here-->
+
+		<!-- Tasks Starts Here -->
+		<div class="all_tasks">
+			<a href="#">Add Tasks</a>
+				<table>
+					<tr>
+						<th>Order</th>
+						<th>Task Name</th>
+						<th>Priority</th>
+						<th>Deadline</th>
+						<th>Actions</th>
+					</tr>
+
+					<tr>
+						<td>1.</td>
+						<td>Design of Website</td>
+						<td>Medium</td>
+						<td>23/05/2020</td>
+						<td>
+							<a href="#">Update</a>
+							<a href="#">Delete</a>
+						</td>
+					</tr>
+				</table>
+		</div>
+		<!-- Tasks Ends Here -->
+	</body>
+</html>
---
+++ PHP_Deliverable/manage_list.php
@@ -0,0 +1,50 @@
+<html>
+	<head>
+		<title>No Shilly-Shally</title>
+	</head>
+
+	<body>
+		<h1>NO SHILLY-SHALLY</h1>
+
+		<!-- Menu Starts Here-->
+		<div class="menu">
+			<a href="index.php">HOME</a>	<!-- Static Link -->
+		</div>
+		<!-- Menu Ends Here-->
+
+		<h3>Manage Lists</h3>
+
+		<!-- Table to Display Lists Starts Here-->
+		<div class="all_lists">
+			<a href="add_lists.php">Add List</a>
+					<table>
+						<tr>
+							<th>Order</th>
+							<th>List Name</th>
+							<th>Actions</th>
+						</tr>
+
+						<!--Dummy Data's Starts Here-->
+						<tr>
+							<td>1.</td>
+							<td>To Do</td>
+							<td>
+								<a href="#">Update</a>
+								<a href="#">Delete</a>
+							</td>
+						</tr>
+
+						<tr>
+							<td>2.</td>
+							<td>In Progress</td>
+							<td>
+								<a href="#">Update</a>
+								<a href="#">Delete</a>
+							</td>
+						</tr>
+						<!--Dummy Data's Ends Here-->
+					</table>
+		</div>
+		<!-- Table to Display Lists Ends Here-->
+	</body>
+</html>
